b4de53980faa58f341cca32c40a1fbd74e63273d
Nicolas BAUW <nbauw@hotmail.com>
2024-05-02 18:44:59 +0200

    Out of bounds R/W protection

--- src/memory.rs
+++ src/memory.rs
@@ -1,4 +1,4 @@
-use std::{fs::File, io::prelude::*,};
+use std::{fs::File, io::prelude::*};
 
 /// The Bus struct is hosting the 8080 memory map and the pending IO operations for outer handling.
 pub struct Bus {
@@ -23,35 +23,64 @@ impl Bus {
 
     /// Sets a ROM space. Write operations will be ineffective in this address range.
     pub fn set_romspace(&mut self, start: u16, end: u16) {
-        self.rom_space = Some(ROMSpace{start, end});
+        self.rom_space = Some(ROMSpace { start, end });
     }
 
     /// Reads a byte from memory
     pub fn read_byte(&self, address: u16) -> u8 {
+        if address as usize >= self.address_space.len() {
+            return 0;
+        }
         self.address_space[usize::from(address)]
     }
 
     /// Writes a byte to memory
     pub fn write_byte(&mut self, address: u16, data: u8) {
+        if address as usize >= self.address_space.len() {
+            return;
+        }
+
         // if rom space is declared, and write operation is requested in rom area : we exit
-        if self.rom_space.is_some() && address >= self.rom_space.as_ref().unwrap().start && address <= self.rom_space.as_ref().unwrap().end { return };
+        if self.rom_space.is_some()
+            && address >= self.rom_space.as_ref().unwrap().start
+            && address <= self.rom_space.as_ref().unwrap().end
+        {
+            return;
+        };
         self.address_space[usize::from(address)] = data;
     }
 
     /// Reads a word stored in memory in little endian byte order, returns this word in BE byte order
     pub fn read_word(&self, address: u16) -> u16 {
-        u16::from(self.address_space[usize::from(address)]) | (u16::from(self.address_space[usize::from(address + 1)]) << 8)
+        if address as usize >= self.address_space.len() {
+            return 0;
+        }
+        u16::from(self.address_space[usize::from(address)])
+            | (u16::from(self.address_space[usize::from(address + 1)]) << 8)
     }
 
     // Reads a word stored in memory in little endian byte order, returns this word in LE byte order
     pub fn read_le_word(&self, address: u16) -> u16 {
-        u16::from(self.address_space[usize::from(address)]) << 8 | (u16::from(self.address_space[usize::from(address + 1)]))
+        if address as usize >= self.address_space.len() {
+            return 0;
+        }
+        u16::from(self.address_space[usize::from(address)]) << 8
+            | (u16::from(self.address_space[usize::from(address + 1)]))
     }
 
     /// Writes a word to memory in little endian byte order
     pub fn write_word(&mut self, address: u16, data: u16) {
+        if address as usize >= self.address_space.len() {
+            return;
+        }
+
         // if rom space is declared, and write operation is requested in rom area : we exit
-        if self.rom_space.is_some() && address >= self.rom_space.as_ref().unwrap().start && address <= self.rom_space.as_ref().unwrap().end { return };
+        if self.rom_space.is_some()
+            && address >= self.rom_space.as_ref().unwrap().start
+            && address <= self.rom_space.as_ref().unwrap().end
+        {
+            return;
+        };
         self.address_space[usize::from(address)] = (data & 0xFF) as u8;
         self.address_space[usize::from(address + 1)] = (data >> 8) as u8;
     }
